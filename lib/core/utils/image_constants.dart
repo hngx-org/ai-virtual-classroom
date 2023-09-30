00c42b3c5f9a0afda1ea9b7ffd85d92a6d8bd5b5
IBRAHIM ABDULLAHI <nsexpress.academy@gmail.com>
2023-10-01 00:14:52 +0100

    default folder setup

---
+++ lib/core/utils/image_constants.dart
@@ -0,0 +1,21 @@
+class ImageConstant {
+  // Image folder path
+  static String imagePath = 'assets/images';
+
+  // Welcome Screen One images
+  static String imgGradesbro1 = '$imagePath/img_gradesbro1.svg';
+
+  // On Boarding images
+  static String img76816ba190b04 = '$imagePath/img_76816ba190b04.png';
+
+  // Sign In images
+  static String imgKnowledgecuate = '$imagePath/img_knowledgecuate.svg';
+
+  // Sign up images
+  static String imgWebinarpana1 = '$imagePath/img_webinarpana1.svg';
+
+  // Common images
+  static String imgEye = '$imagePath/img_eye.svg';
+
+  static String imageNotFound = 'assets/images/image_not_found.png';
+}
