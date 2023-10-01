2888e408e1c7a9a5a7cea6d0106c81a16729b95e
Mark <kmark041@gmail.com>
2023-10-01 01:24:37 +0100

    First Commit

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -1,15 +1,23 @@
+import 'package:ai_virtual_classroom/themes/app_colors.dart';
 import 'package:flutter/material.dart';
 
-class AppTheme {
-  Color appBackgroundColor = const Color(0xffEEEFF2);
-  Color navBackgroundColor = const Color(0xFFEFECFF);
-  Color primaryColor = const Color(0xFF150D57);
-  Color primary600 = const Color(0xFF523FE7);
-  Color appButtonTextColor = const Color(0XFFCBFF89);
-  Color black200 = const Color(0XFF8A8A8A);
-  Color white = const Color(0xffffffff);
-  Color bottomNavBarBG = const Color(0XFFEFECFF);
-  static const Color btnBgColor = Color.fromRGBO(21, 13, 87, 1);
-  static const Color btnTextColor = Color.fromARGB(203, 255, 137, 1);
-  static const Color checkBoxMarkColor = Color(0xffBEFF6C);
-}
+ThemeData themeData = ThemeData(
+  fontFamily: 'ExoRoman',
+  useMaterial3: true,
+  primaryColor: AppTheme().primaryColor,
+  bottomNavigationBarTheme: bottomNav,
+  appBarTheme: appBarTheme(),
+).copyWith(
+  scaffoldBackgroundColor: AppTheme().appBackgroundColor,
+);
+
+BottomNavigationBarThemeData bottomNav = BottomNavigationBarThemeData(
+  elevation: 1,
+  backgroundColor: AppTheme().bottomNavBarBG,
+);
+AppBarTheme appBarTheme() => AppBarTheme(
+      elevation: 0,
+      centerTitle: true,
+      backgroundColor: AppTheme().white,
+      iconTheme: IconThemeData(color: AppTheme().black200),
+    );
